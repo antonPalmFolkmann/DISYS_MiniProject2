38809810a70c2bd2bf458f1fc37455e89841a77b
ldej <ldej@itu.dk>
2021-10-12 19:52:16 +0200

    gofiles does not compile...

--- Client/Participant.go
+++ Client/Participant.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"context"
+	"fmt"
+
+	"log"
+
+	"github.com/antonPalmFolkmann/DISYS_MiniProject2.git/ChatService"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	// Creat a virtual RPC Client Connection on port  9080 WithInsecure (because  of http)
+	var conn *grpc.ClientConn
+	conn, err := grpc.Dial(":9080", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Could not connect: %s", err)
+	}
+
+	// Defer means: When this function returns, call this method (meaing, one main is done, close connection)
+	defer conn.Close()
+
+	//  Create new Client from generated gRPC code from proto
+	c := ChatService.ChittyChatServiceClient(conn)
+
+	SendRequest(c)
+}
+
+func SendRequest(c ChatService.ChittyChatServiceClient) {
+	// Between the curly brackets are nothing, because the .proto file expects no input.
+	message := ChatService.PublishMessageRequest{}
+
+	response, err := c.Publish(context.Background(), &message)
+	if err != nil {
+		log.Fatalf("Error when calling Publish: %s", err)
+	}
+
+	fmt.Printf("Response from the Server: %s \n", response.Reply)
+}
--- Server/ChatServer.go
+++ Server/ChatServer.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/antonPalmFolkmann/DISYS_MiniProject2.git/ChatService"
+
+	"google.golang.org/grpc"
+)
+
+type Server struct {
+	ChatService.UnimplementedChittyChatServiceServer
+}
+
+func (s *Server) BroadCast(ctx context.Context, in *ChatService.ChatMessage) (*ChatService.PublishMessage, error) {
+	fmt.Printf("Received broadcastrequest request")
+	return &ChatService.PublishMessage{Reply: ChatService.ChatMessage.Message}, nil
+}
+
+func main() {
+	// Create listener tcp on port 9080
+	list, err := net.Listen("tcp", ":9080")
+	if err != nil {
+		log.Fatalf("Failed to listen on port 9080: %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	ChatService.RegisterChittyChatServiceServer(grpcServer, &Server{})
+
+	if err := grpcServer.Serve(list); err != nil {
+		log.Fatalf("failed to server %v", err)
+	}
+}
